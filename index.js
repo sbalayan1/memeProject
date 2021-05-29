c1592dc051fbe4f555e53e6a859a986074f780e1
sbalayan1 <balayans2014@yahoo.com>
2021-05-29 16:13:36 -0700

    changed the submit button color and added a click event listener for the navigation bar

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let makeAMemeButton = document.querySelector('.makeAMeme')
 let memeNameList = document.querySelector('.listOfMemes')
 let collapsibleList = document.getElementsByClassName("collapsible");
 
-//gets the second meme in the API and loads it on the page on domcontentload
+//gets the 10th meme in the API and loads it on the page on domcontentload
 let loadApiImage = () => {
     fetch(apiURL)
     .then(res => res.json())
@@ -36,9 +36,20 @@ let loadMemeNames = () => {
     .then(element => element.data.memes.forEach((meme)=>{
         let li = document.createElement('li')
         li.textContent = meme.name
+
+        li.id = meme.name
+
         memeNameList.append(li)
-        
-    } ))
+
+         //event listener that allows the user to select a meme from the navigation bar
+        li.addEventListener('click', () => {
+        console.log('you clicked on a meme in the list?')       
+        memeHeader.textContent = meme.name
+        memeImage.src = meme.url
+
+            })
+        })
+    )
 }
 
 
@@ -59,7 +70,8 @@ for (i = 0; i < collapsibleList.length; i++) {
 }
 }
 
-//button click event listener 
+ 
+//add content to your meme click event listener 
 makeAMemeButton.addEventListener('submit', (e) => {
     e.preventDefault()
     console.log('you have been clicked')
